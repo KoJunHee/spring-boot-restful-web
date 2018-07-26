ccd277d0f947e2b41f39d4bc6da4904a065a971b
Junhee Ko <kjh338076@gmail.com>
2018-07-26 14:15:50 -0700

    added post

--- src/main/java/com/example/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,43 @@
 package com.example.rest.webservices.restfulwebservices.user;
 
-import java.util.Date;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@ApiModel(description = "All details about the user")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
+
+    @Size(min=2, message="Name should have at least 2 characters")
+    @ApiModelProperty(notes="Name should have at least 2 characters")
     private String name;
+
+    @Past
+    @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 
     protected User() {
     }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values(1, sysdate(), 'AB');
+insert into user values(2, sysdate(), 'Junhe');
+insert into user values(3, sysdate(), 'KO');
+
+insert into post values(11001, 'My first post', 1);
+insert into post values(11002, 'My second post', 1);
